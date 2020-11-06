73fc01f0ec46dc2382cbf113605150861d1e8e0c
mmglove <gmm_email@163.com>
2020-11-06 15:04:04 +0800

    add slim rec ce path

---
+++ ce/python3.7_linux_gpu_train_slim/p0_slim_quant_aware_v1/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# encoding=utf-8 vi:ts=4:sw=4:expandtab:ft=python
+
+"""
+Copyright (c) 2020 Baidu.com, Inc. All Rights Reserved
+File: __init__.py.py
+
+Authors: guomengmeng01(guomengmeng01@baidu.com)
+Date: 2020/11/6 15:02
+"""
